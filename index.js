318d20f4db3bb4345bf02bf320eb0caf24d0f7b0
Roger Sun <rogersun@outlook.com>
2018-06-12 04:11:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ function sayHiToGrandma(string){
     else if (string.toLowerCase() === string){
       return("I can't hear you!")
     }
-    else{return("I love you, Grandma.")
+    else{return("I love you, too.")
     }
 }
